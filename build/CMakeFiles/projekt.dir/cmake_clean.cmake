2551108877a9e7a641686b496d254cbf39283afa
michal-skoczylas <privmichalskoczylas@gmail.com>
2024-05-21 22:56:56 +0200

    Dalszy szkielet projektu

---
+++ build/CMakeFiles/projekt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt.dir/src/main.cpp.o"
+  "CMakeFiles/projekt.dir/src/main.cpp.o.d"
+  "projekt"
+  "projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
